4bab6661b8a43fc87a0e0fd1ba831efa0e1feff8
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-03-30 23:55:07 +0300

    jsBasic

--- FCC/JSBasic.js
+++ FCC/JSBasic.js
@@ -1,3 +1,112 @@
 /* const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine' // Change this line
 console.log(myStr)
  */
+// Setup
+/* let myStr = 'Jello World'
+
+// Only change code below this line
+myStr = 'Hello World' // Change this line
+// Only change code above this line
+console.log(myStr)
+ */
+
+/* // Setup
+const myArray = [
+	['John', 23],
+	['cat', 2],
+]
+
+// Only change code below this line
+myArray.push(['dog', 3])
+ */
+
+/* // Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift() */
+/* 
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]) */
+/* const myList = []
+console.log(
+	myList.concat([
+		['Bread', 1],
+		['Milk', 2],
+		['Water', 3],
+		['Chocolate', 4],
+		['Cookies', 5],
+	])
+)
+ */
+/* function reusableFunction (){console.log("Hi World")}
+reusableFunction () */
+/* function functionWithArgs(a, b){console.log(a+b)}
+functionWithArgs(1,2) */
+/* function timesFive(a) {
+	return a * 5
+}
+ */
+/* // Declare the myGlobal variable below this line
+const myGlobal = 10
+function fun1() {
+	// Assign 5 to oopsGlobal here
+	oopsGlobal = 5
+}
+
+// Only change code above this line
+
+function fun2() {
+	let output = ''
+	if (typeof myGlobal != 'undefined') {
+		output += 'myGlobal: ' + myGlobal
+	}
+	if (typeof oopsGlobal != 'undefined') {
+		output += ' oopsGlobal: ' + oopsGlobal
+	}
+	console.log(output)
+} */
+
+// Setup
+/* const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+let outerWear= 'sweater'
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit(); */
+/* 
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+sum+=5
+}
+
+// Only change code above this line
+
+addThree();
+addFive(); */
+/* 
+// Setup
+let processed = 0
+
+function processArg(num) {
+	return (num + 3) / 5
+}
+
+// Only change code below this line
+processed = processArg(7)
+ */
