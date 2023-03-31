af3c3bdeae7d61ce2ffd216eaa59875af98be765
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-03-31 18:08:48 +0300

    html

--- FCC/es6.js
+++ FCC/es6.js
@@ -71,3 +71,54 @@ console.log(d)
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const sourceWithoutFirstTwo = removeFirstTwo(source)
  */
+/* import subtract from './math_functions.js'
+// Only change code above this line
+
+subtract(7, 4)
+ */
+/* const makeServerRequest = new Promise((resolve, reject) => {
+	// responseFromServer represents a response from a server
+	let responseFromServer;
+	  
+	if(responseFromServer) {
+	  resolve ("We got the data")
+	  // Change this line
+	} else {  
+	 reject ("Data not received")
+	  // Change this line
+	}
+ }); */
+/* const makeServerRequest = new Promise((resolve, reject) => {
+	// responseFromServer is set to true to represent a successful response from a server
+	let responseFromServer = true
+
+	if (responseFromServer) {
+		resolve('We got the data')
+	} else {
+		reject('Data not received')
+	}
+})
+
+makeServerRequest.then((result) => console.log(result))
+ */
+/* const makeServerRequest = new Promise((resolve, reject) => {
+	// responseFromServer is set to false to represent an unsuccessful response from a server
+	let responseFromServer = false
+
+	if (responseFromServer) {
+		resolve('We got the data')
+	} else {
+		reject('Data not received')
+	}
+})
+
+makeServerRequest
+	.then((result) => {
+		console.log(result)
+	})
+	.catch((error) => console.log(error))
+ */
+
+/* 	let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line */
