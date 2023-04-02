68f41a44218139ddcaf1bddb38778aedecf0287f
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-04-02 08:34:45 +0300

    02.04.23

--- FCC/JSBasic.js
+++ FCC/JSBasic.js
@@ -494,3 +494,72 @@ if (current === 'green') {
 	return 'green'
 }
  */
+/* function checkObj(obj, checkProp) {
+	// Only change code below this line
+  if( obj.hasOwnProperty(checkProp)){return obj[checkProp]}
+	return "Not Found";
+	// Only change code above this line
+ } */
+/* const myStorage = {
+	car: {
+		inside: {
+			'glove box': 'maps',
+			'passenger seat': 'crumbs',
+		},
+		outside: {
+			trunk: 'jack',
+		},
+	},
+}
+
+const gloveBoxContents = myStorage.car.inside['glove box']
+ */
+/* const myPlants = [
+	{
+		type: 'flowers',
+		list: ['rose', 'tulip', 'dandelion'],
+	},
+	{
+		type: 'trees',
+		list: ['fir', 'pine', 'birch'],
+	},
+]
+
+const secondTree = myPlants[1].list[1]
+console.log(secondTree)
+ */
+// Setup
+const recordCollection = {
+	2548: {
+		albumTitle: 'Slippery When Wet',
+		artist: 'Bon Jovi',
+		tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+	},
+	2468: {
+		albumTitle: '1999',
+		artist: 'Prince',
+		tracks: ['1999', 'Little Red Corvette'],
+	},
+	1245: {
+		artist: 'Robert Palmer',
+		tracks: [],
+	},
+	5439: {
+		albumTitle: 'ABBA Gold',
+	},
+}
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+	console.log(records.id)
+	/* 	if (value == '') {
+		delete records.id.prop
+	} */
+	/* if (prop === records.tracks && value !== '') {
+			records = records.tracks[]
+		} */
+
+	return records
+}
+
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'))
