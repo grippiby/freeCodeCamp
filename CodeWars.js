1285239f9014f90b19e6868c789ae2f7d1c02a54
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-04-02 11:03:30 +0300

    02.04.23

--- CodeWars.js
+++ CodeWars.js
@@ -13,3 +13,25 @@
 }
 console.log(consecutive([4, 8, 6]))
  */
+/* var humanYearsCatYearsDogYears = function (humanYears) {
+	// Your code here!
+	let cat = 0
+	let dog = 0
+	if (humanYears >= 3) {
+		for (let i = humanYears; i >= 3; i--) {
+			cat += 4
+			dog += 5
+		}
+		cat = cat + 24
+		dog = dog + 24
+	} else if (humanYears == 1) {
+		return [humanYears, 15, 15]
+	} else if (humanYears == 2) {
+		return [humanYears, 24, 24]
+	}
+	return [humanYears, cat, dog]
+}
+console.log(humanYearsCatYearsDogYears(10))
+console.log(humanYearsCatYearsDogYears(1))
+console.log(humanYearsCatYearsDogYears(2))
+ */
--- FCC/es6.js
+++ FCC/es6.js
@@ -122,3 +122,56 @@ makeServerRequest
 /* 	let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line */
+/* 
+const result = {
+	success: ['max-length', 'no-amd', 'prefer-arrow-functions'],
+	failure: ['no-var', 'var-on-top', 'linebreak'],
+	skipped: ['no-extra-semi', 'no-dup-keys'],
+}
+function makeList(arr) {
+	// Only change code below this line
+	const failureItems = []
+
+	arr.forEach((element) => {
+		console.log(element)
+		failureItems.push(`<li class="text-warning">${element}</li>`)
+	})
+	console.log(failureItems)
+	return failureItems
+}
+
+const failuresList = makeList(result.failure)
+ */
+// Only change code below this line
+/* class Vegetable  {
+	constructor(name) {
+	  this.name = name;
+	}
+ }
+ 
+ // Only change code above this line
+ 
+ const carrot = new Vegetable('carrot');
+ console.log(carrot.name); // Should display 'carrot' */
+
+/* 
+// Only change code below this line
+class Thermostat {
+	constructor(degr) {
+		this._degr = degr
+	}
+
+	get temperature() {
+		return (5 / 9) * (this._degr - 32)
+	}
+
+	set temperature(value) {
+		this._degr = (value * 9.0) / 5 + 32
+	}
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76) // Setting in Fahrenheit scale
+let temp = thermos.temperature // 24.44 in Celsius
+thermos.temperature = 26
+temp = thermos.temperature // 26 in Celsius */
