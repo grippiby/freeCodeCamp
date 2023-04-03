cfb0d36d107c07881a6d92db897c2542f11812d2
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-04-03 14:50:59 +0300

    second commit of that day

--- FCC/JSBasic.js
+++ FCC/JSBasic.js
@@ -604,3 +604,55 @@ for (let i = 10; i > 0; i -= 2) {
 console.log(myArray)
 // Only change code below this line
  */
+
+/* // Setup
+const myArray = []
+
+for (let i = 9; i > 0; i -= 2) {
+	myArray.push(i)
+}
+console.log(myArray)
+// Only change code below this line
+ */
+/* // Setup
+let total = 0
+const myArr = [2, 3, 4, 5, 6];
+for (let i = 0; i< myArr.length; i ++) {
+  total+=myArr[i]
+}
+// Only change code below this line
+ */
+/* function multiplyAll(arr) {
+	let product = 1;
+	// Only change code below this line
+ for (let i = 0; i < arr.length; i++) {
+	for (let j = 0; j < arr[i].length; j++) {
+	
+	product*=  arr[i][j]
+	}
+ }
+	// Only change code above this line
+	return product;
+ }
+ 
+ multiplyAll([[1, 2], [3, 4], [5, 6, 7]]); */
+// Setup
+/* // Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 11); */
+/* function sum(arr, n) {
+	// Only change code below this line
+	if (n === 0) {
+		return 0
+	} else {
+		return sum(arr, n - 1) + arr[n - 1]
+	}
+}
+// Only change code above this line
+ */
