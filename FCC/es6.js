2fc9723a93b03f9cfceaae7cc81cf642f3dc6d2b
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-04-06 12:27:17 +0300

    first commit

--- FCC/es6.js
+++ FCC/es6.js
@@ -175,3 +175,13 @@ const thermos = new Thermostat(76) // Setting in Fahrenheit scale
 let temp = thermos.temperature // 24.44 in Celsius
 thermos.temperature = 26
 temp = thermos.temperature // 26 in Celsius */
+/* function checkScope() {
+	let i = 'function scope'
+	if (true) {
+		let i = 'block scope'
+		console.log('Block scope i is: ', i)
+	}
+	console.log('Function scope i is: ', i)
+	return i
+}
+checkScope() */
