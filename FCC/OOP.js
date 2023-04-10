ee9a78570b4f2cb98f8b909d58a21750e027d2af
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-04-10 22:52:44 +0300

    2nd commit

---
+++ FCC/OOP.js
@@ -0,0 +1,42 @@
+/* let dog = {
+	name: 'Aflac',
+	numLegs: 2,
+}
+let dog = {
+	name: 'Spot',
+	numLegs: 4,
+}
+// Only change code below this line
+console.log(dog.name)
+console.log(dog.numLegs) */
+
+/* let dog = {
+	name: 'Spot',
+	numLegs: 4,
+	sayLegs() {
+		return 'This dog has ' + dog.numLegs + ' legs'
+	},
+}
+
+console.log(dog.sayLegs())
+ */
+/* let dog = {
+	name: 'Spot',
+	numLegs: 4,
+	sayLegs() {
+		return `This dog has ${dog.numLegs} legs`
+	},
+}
+
+console.log(dog.sayLegs())
+ */
+/* let dog = {
+	name: 'Spot',
+	numLegs: 4,
+	sayLegs: function () {
+		return 'This dog has ' + this.numLegs + ' legs.'
+	},
+}
+
+dog.sayLegs()
+ */
